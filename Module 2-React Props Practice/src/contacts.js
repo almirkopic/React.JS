10e06c7d9478ffb14196605b52d426aa314b1c11
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2023-08-26 16:11:02 +0200

    Add files via upload

---
+++ Module 2-React Props Practice/src/contacts.js
@@ -0,0 +1,27 @@
+import App from "./components/App";
+
+const contacts = [
+  {
+    name: "Ben",
+    imgURL:
+      "https://yt3.googleusercontent.com/ytc/AOPolaSeen9o_imlen2obzml4dRKBQ91gXlSlwEaO2TpBQ=s900-c-k-c0x00ffffff-no-rj",
+    phone: "+123 456 789",
+    email: "b@ben.com"
+  },
+  {
+    name: "Jack",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    name: "Mike",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@Mike.com"
+  }
+];
+
+export default contacts;
