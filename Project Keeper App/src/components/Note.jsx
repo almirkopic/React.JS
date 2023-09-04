0ab5e3dd924a549f8015efdf4889d3b41c8eeb76
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2023-09-04 19:53:38 +0200

    Update Note.jsx

--- Project Keeper App/src/components/Note.jsx
+++ Project Keeper App/src/components/Note.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-function Note() {
+function Note(props) {
 
 return <div className="note">
-    <h1> This is the Tittle</h1>
-    <p>Content here</p>
+    <h1>{props.title}</h1>
+    <p>{props.content}</p>
 </div>
 
 
